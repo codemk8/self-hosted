a4334bf5f41e29cf15124d33ca5408350f964d44
Code M <codemk8@gmail.com>
2023-02-04 11:21:41 -0500

    Works without nginx

--- volumes.sh
+++ volumes.sh
@@ -1,3 +1,11 @@
+#!/usr/bin/bash
+
 export ROOT=/opt/self-hosted/data
 mkdir -p $ROOT/ldap
 mkdir -p $ROOT/slapd.d
+mkdir -p $ROOT/gitlab/config
+mkdir -p $ROOT/gitlab/data
+mkdir -p $ROOT/gitlab-runner
+
+export GITLAB_HOME=$ROOT/gitlab
+export LDAP_HOME=$ROOT/ldap
